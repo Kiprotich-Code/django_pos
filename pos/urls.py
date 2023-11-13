f7533151fb42ec60a24c0cde0de40466232eea64
Lawi Kiprotich <fleeklawi@gmail.com>
2023-11-13 08:39:26 +0300

    Commit

--- pos/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.dashboard, name='dashboard'),
-    path('billing/', views.billing, name='billing'),
-    path('billing/order', views.order, name='order'),
-]
